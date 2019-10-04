28809a23bc837edf042a6dcbc31b19ed8fb43e6b
Pham Thi Kim Oanh <kimoanh2613@gmail.com>
2019-10-04 20:57:25 +0700

    update react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Tui là Kim Kim</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
